bff9a7e017170d89e75f4185808826ae68b098cc
Jozef Marko <jomarko@redhat.com>
2020-08-05 14:37:04 +0200

    Reduce ReadOnlyProvider propagation

--- kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/api/ReadOnlyProviderImpl.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/api/ReadOnlyProviderImpl.java
@@ -40,6 +40,6 @@ public class ReadOnlyProviderImpl implements ReadOnlyProvider {
 
     @Override
     public boolean isReadOnlyDiagram() {
-        return false;
+        return contextProvider != null && contextProvider.getChannel() == GITHUB;
     }
 }
